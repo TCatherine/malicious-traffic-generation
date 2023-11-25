32872a168398bd39b6b44e647c3632c19c8c9751
tkacheva <ekaterina.tkacheva@sbermegamarket.ru>
2023-11-25 14:29:15 +0300

    fix run

--- aggregator/multiplier/main.py
+++ aggregator/multiplier/main.py
@@ -1,7 +1,7 @@
 from parser import parse, get_strings
 from train import train
 from parameters import *
-from aggregator.multiplier.models.transformer_gan.cgan_model import CGAN_Model
+from aggregator.multiplier.models.basic_gan.cgan_model import CGAN_Model
 
 import torch
 
@@ -13,17 +13,18 @@ def is_cuda() -> bool:
 def train_cgan(dataset):
     x, y = dataset
     model = CGAN_Model(hidden_sz=x.shape[1:])
-    # model.fit(x, y)
-    # model.plot_loss()
+    model.fit(x, y)
+    model.plot_loss()
     return model
 
 def main():
     tokenizer, dataset = parse(bpe_params = BPE)
     model = train_cgan(dataset)
     data_shape = dataset[0].shape
-    data_shape = (2, data_shape[1], data_shape[2])
+    data_shape = (1, data_shape[1], data_shape[2])
     res = model.generate(data_shape).tolist()
     url = get_strings(res, tokenizer)
+    a = 1
 
 if __name__ == "__main__":
     main()
